3f378956ffb3ed946ecc2ec405e07a54dd54942f
isongjosiah <isongjosiah@gmail.com>
2020-04-17 19:51:16 +0100

    Day 9

---
+++ Go_strings/Strings_package/useStrings.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"fmt"
+	s "strings"
+)
+
+var f = fmt.Printf
+
+func main() {
+	upper := s.ToUpper("Hello there!")
+	f("To Upper: %s\n", upper)
+	f("To Lower: %s\n", s.ToLower("Hello THERE"))
+	f("%s\n", s.Title("tHis wiLL be A title!"))
+	f("EqualFold: %v\n", s.EqualFold("Mihalis", "MIHAlis"))
+	f("EqualFold: %v\n", s.EqualFold("Mihalis", "MIHAli"))
+
+	f("Prefix: %v\n", s.HasPrefix("Mihalis", "Mi"))
+	f("Prefix: %v\n", s.HasPrefix("Mihalis", "mi"))
+	f("Suffix: %v\n", s.HasSuffix("MIhalis", "is"))
+	f("Suffix: %v\n", s.HasSuffix("Mihalis", "IS"))
+
+	f("Index: %v\n", s.Index("Mihalis", "ha"))
+	f("Index: %v\n,", s.Index("Mihalis", "HA"))
+	f("Count: %v\n", s.Count("Mihalis", "i"))
+	f("Count: %v\n", s.Count("Mihalis", "I"))
+	f("Repeat: %s\n", s.Repeat("ab", 5))
+
+	f("TrimSpace: %s\n", s.TrimSpace("\tThis is a line. \n"))
+	f("TrimLeft: %s", s.TrimLeft("\tThis is a \t line. \n", "\n\t"))
+	f("TrimRight: %s\n", s.TrimRight("\tThis is a \t line. \n", "\n\t "))
+
+}
