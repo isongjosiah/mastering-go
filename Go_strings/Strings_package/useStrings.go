4fa89398c783a703c96da91f199b122c52c159fb
isongjosiah <isongjosiah@gmail.com>
2020-04-18 19:28:17 +0100

    Day 10

--- Go_strings/Strings_package/useStrings.go
+++ Go_strings/Strings_package/useStrings.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	s "strings"
+	"unicode"
 )
 
 var f = fmt.Printf
@@ -30,4 +31,27 @@ func main() {
 	f("TrimLeft: %s", s.TrimLeft("\tThis is a \t line. \n", "\n\t"))
 	f("TrimRight: %s\n", s.TrimRight("\tThis is a \t line. \n", "\n\t "))
 
+	f("Compare: %v\n", s.Compare("Mihalis", "MIHALIS"))
+	f("Compare: %v\n", s.Compare("Mihalis", "Mihalis"))
+	f("Compare: %v\n", s.Compare("MIHALIS", "MIHalis"))
+
+	f("Fields: %v\n", s.Fields("This is a string!"))
+	f("Fields: %v\n", s.Fields("Thisis\na\tstring"))
+
+	f("%s\n", s.Split("abcd efg", ""))
+
+	f("%s\n", s.Replace("abcd efg", " ", "_", -1))
+	f("%s\n", s.Replace("abcd efg", " ", "_", 4))
+	f("%s\n", s.Replace("abcd efg hij klm nop", " ", "_", 2))
+
+	lines := []string{"Line 1", "Line 2", "Line 3"}
+	f("John: %s\n", s.Join(lines, "+++"))
+
+	f("SplitAfter: %s\n", s.SplitAfter("123++432++", "++"))
+
+	trimFunction := func(c rune) bool {
+		return !unicode.IsLetter(c)
+	}
+
+	f("TrimFunc: %s\n", s.TrimFunc("123 abc ABC \t", trimFunction))
 }
